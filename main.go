0fa9043f86bb19c78558af20b87aa06f290e77b4
Rota-of-light <Jeremiah.C.Applebee@gmail.com>
2025-04-09 01:35:03 -0700

    Creation of message database

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "github.com/lib/pq" //Needed for side-effect
 	"github.com/joho/godotenv"
+	"github.com/google/uuid"
 
 	"os"
 	"database/sql"
@@ -12,6 +13,7 @@ import (
 	"fmt"
 	"encoding/json"
 	"strings"
+	"time"
 
 	"github.com/Rota-of-light/HTTPServer/internal/database"
 )
@@ -19,6 +21,22 @@ import (
 type apiConfig struct {
 	db		*database.Queries
 	fileserverHits atomic.Int32
+	platform	string
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+type Chirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body     string    `json:"body"`
+	UserId	 uuid.UUID    `json:"user_id"`
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -49,11 +67,55 @@ func (cfg *apiConfig) metricCountHandler(w http.ResponseWriter, r *http.Request)
 	w.Write([]byte(res))
 }
 
-func (cfg *apiConfig) metricResetHandler(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) adminResetHandler(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("I can't let you do that."))
+		return
+	}
+	err := cfg.db.Reset(r.Context())
+    if err != nil {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Something went wrong when attempting to delete all users"))
+		return
+    }
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	cfg.fileserverHits.Store(0)
 	w.Write([]byte("Reset successful!"))
+}
+
+func (cfg *apiConfig) createUserHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+        Email string `json:"email"`
+    }
+	if r.Body == nil {
+		respondWithError(w, http.StatusBadRequest, "Request body missing")
+		return
+	}
+    decoder := json.NewDecoder(r.Body)
+    params := parameters{}
+    err := decoder.Decode(&params)
+    if err != nil {
+		errorString := "Something went wrong"
+        respondWithError(w, http.StatusInternalServerError, errorString)
+		return
+    }
+	user, err := cfg.db.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		errorString := "Something went wrong when attempting to create user"
+        respondWithError(w, http.StatusInternalServerError, errorString)
+		return
+	}
+	newUser := User{
+		ID:	user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email: user.Email,
+	}
+	respondWithJSON(w, http.StatusCreated, newUser)
 }
 
 func profaneChecker(s string) string {
@@ -106,9 +168,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Write(data)
 }
 
-func validateHandler(w http.ResponseWriter, r *http.Request){
+func (cfg *apiConfig) chirpsHandler(w http.ResponseWriter, r *http.Request){
     type parameters struct {
         Body string `json:"body"`
+		UserId uuid.UUID `json:"user_id"`
     }
 	if r.Body == nil {
 		respondWithError(w, http.StatusBadRequest, "Request body missing")
@@ -118,8 +181,14 @@ func validateHandler(w http.ResponseWriter, r *http.Request){
     params := parameters{}
     err := decoder.Decode(&params)
     if err != nil {
-		errorString := "Something went wrong"
+		errorString := "Something went wrong when decoding request"
         respondWithError(w, http.StatusInternalServerError, errorString)
+		return
+    }
+	user, err := cfg.db.GetUserByID(r.Context(), params.UserId)
+	if len(user.ID) == 0 || err != nil {
+		errorString := "Invalid user ID"
+        respondWithError(w, http.StatusBadRequest, errorString)
 		return
     }
     if len(params.Body) > 140 {
@@ -128,13 +197,24 @@ func validateHandler(w http.ResponseWriter, r *http.Request){
 		return
 	}
 	cleanedString := profaneChecker(params.Body)
-	type returnString struct {
-		CleanedBody string `json:"cleaned_body"`
+	chirpParam := database.CreateChirpParams{
+		Body:   cleanedString,
+		UserID: user.ID,
 	}
-	resString := returnString{
-		CleanedBody: cleanedString,
+	chirpRes, err := cfg.db.CreateChirp(r.Context(), chirpParam)
+	if err != nil {
+		errorString := "Error when attempting to create chirp"
+        respondWithError(w, http.StatusInternalServerError, errorString)
+		return
+    }
+	chirpJSON := Chirp{
+		ID: chirpRes.ID,
+		CreatedAt: chirpRes.CreatedAt,
+		UpdatedAt: chirpRes.UpdatedAt,
+		Body: chirpRes.Body,
+		UserId: chirpRes.UserID,
 	}
-    respondWithJSON(w, http.StatusOK, resString)
+    respondWithJSON(w, http.StatusCreated, chirpJSON)
 }
 
 func main() {
@@ -150,6 +230,7 @@ func main() {
 	dbQueries := database.New(db)
 	config := &apiConfig{
 		db: dbQueries,
+		platform: os.Getenv("PLATFORM"),
 	}
 	server := http.NewServeMux()
 	server.HandleFunc("GET /api/healthz", healthCheckHandler)
@@ -157,8 +238,9 @@ func main() {
 	fServer := http.FileServer(dir)
 	server.Handle("/app/", config.middlewareMetricsInc(http.StripPrefix("/app", fServer)))
 	server.HandleFunc("GET /admin/metrics", config.metricCountHandler)
-	server.HandleFunc("POST /admin/reset", config.metricResetHandler)
-	server.HandleFunc("POST /api/validate_chirp", validateHandler)
+	server.HandleFunc("POST /admin/reset", config.adminResetHandler)
+	server.HandleFunc("POST /api/users", config.createUserHandler)
+	server.HandleFunc("POST /api/chirps", config.chirpsHandler)
 	s := &http.Server{
 		Addr:	":8080",
 		Handler: server,
